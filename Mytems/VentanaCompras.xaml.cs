a07ff64d4cf2df1abf2bf7f37d6120ef9959a915
Alumno <Alumno@PC23>
2019-11-05 12:27:45 -0600

    Ventana Compras Oscar

---
+++ Mytems/VentanaCompras.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mytems
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaCompras.xaml
+    /// </summary>
+    public partial class VentanaCompras : Window
+    {
+        public VentanaCompras()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBuyKnife_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CbAcc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
