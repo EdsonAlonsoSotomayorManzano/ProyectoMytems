610dfda6ef2a19eb19ca03a0ad64b6c3e2095d87
Edson Sotomayor <esotomayor0@ucol.mx>
2019-11-11 17:33:28 -0600

    v4

--- Mytems/MainWindow.xaml.cs
+++ Mytems/MainWindow.xaml.cs
@@ -36,5 +36,10 @@ namespace Mytems
             lobby.Show();
             this.Close();
         }
+
+        private void BtnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
