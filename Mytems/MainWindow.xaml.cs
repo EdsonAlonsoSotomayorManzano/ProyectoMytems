fb8bba36c74b0c74a9cea6b34d45b073759eccdf
Edson Sotomayor <esotomayor0@ucol.mx>
2019-11-23 23:59:02 -0600

    v2

--- Mytems/MainWindow.xaml.cs
+++ Mytems/MainWindow.xaml.cs
@@ -25,63 +25,67 @@ namespace Mytems
     public partial class MainWindow : Window
     {
 
-     
-        
+        OleDbConnection con;
+
         public MainWindow()
         {
+       
             InitializeComponent();
-           
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\MytemsDB.mdb";
+         
         }
-        OleDbConnection Conexion = new OleDbConnection ("Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\MytemsDB.mdb");
+        
 
         private void btnFPass_Click(object sender, RoutedEventArgs e)
         {
-
+            
+            
         }
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
 
         {
-                                                  
-            Conexion.Open();
-
-            //: Aquí especificamos los campos a tomar con sus textbox correspondientes
-            String Consulta = "select Pass, Email from Registrer where txtPassword ='" + txtPassword.Text + "' and txtUsarname ='" + txtUsarname.Text + "'";
-
-            //: Aquí Creamos una variable para unir la consulta y la conexion
-            OleDbCommand Comando = new OleDbCommand(Consulta, Conexion);
-
-            //:Creamos una variable para leer los datos
-            OleDbDataReader LectorDatos;
-
-            //: Aquí ejecutamos el lector de datos
-            LectorDatos = Comando.ExecuteReader();
-
-            //: Aquí creamos una variable booleana para ver si existe el registro
-            bool ExistenciaRegistros = LectorDatos.HasRows;
-            if (ExistenciaRegistros)
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            string User = this.txtUsarname.Text;
+            string Password = this.txtPassword.Text;
+            if (User == "" || Password == "")
             {
-                //: SI existe el registro mostrar el form siguiente
-                MessageBox.Show("Bienvenido " + txtUsarname.Text);
-
-
-                Register Regiswindow = new Register();
-                Regiswindow.Show();
-                this.Close();
+                MessageBox.Show("Insert User Name and Password");
+                this.txtUsarname.Focus();
+                return;
             }
-
             else
             {
-
-                //: SI no existe volver al paso de logueo
-                MessageBox.Show("Acceso denegado " + txtUsarname.Text, "Usuario NO encontrado");
-
-
-                return;
+                OleDbConnection oleDbConnection = new OleDbConnection();
+                if (txtUsarname.Text != "")
+                {
+                    if (txtUsarname.IsEnabled == true)
+                    {
+                        cmd.CommandText = "select [id_Usuario], from Register where[Us]= " + this.txtUsarname + "and[Pass]= " + this.txtPassword + "";
+                        if (txtPassword != ' [Pass] ')
+                        {
+                            MessageBox.Show("The Password is incorrect");
+                        }
+                        else
+                        {
+                            Home Home = new Home();
+                            this.Hide();
+                            Home.Show();
+                            MessageBox.Show("Welcome Pleyer :D");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Empty field");
+                }
 
             }
         }
-
         private void BtnRegister_Click(object sender, RoutedEventArgs e)
         {
             Register Regiswindow = new Register();
