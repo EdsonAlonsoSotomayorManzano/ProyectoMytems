85928eb54b1d4193797ccb802c5df2e5d430e682
Alumno <Alumno@PC22>
2019-11-28 09:28:36 -0600

    V comfirm trading

---
+++ Mytems/Confirm-trading.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mytems
+{
+    /// <summary>
+    /// Lógica de interacción para Confirm_trading.xaml
+    /// </summary>
+    public partial class Confirm_trading : Window
+    {
+        public Confirm_trading()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtSerch_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CbAcc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void btnShopCar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBuyKnife_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MYCOINS_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
